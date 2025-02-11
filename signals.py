3aec6190192efd9e0376932b381013137d38234c
Melony-0069 <clenvlaovo@gmail.com>
2025-02-11 17:20:20 +0300

    Add files via upload

--- signals.py
+++ signals.py
@@ -1,3 +1,6 @@
 from django.dispatch import Signal
 
-connection_created = Signal()
+request_started = Signal()
+request_finished = Signal()
+got_request_exception = Signal()
+setting_changed = Signal()
