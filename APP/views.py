f5dcd998a7b74e78baa34fc68b7a0ee404de7bc7
Melony-0069 <clenvlaovo@gmail.com>
2025-02-11 16:53:07 +0300

    Add files via upload

---
+++ APP/views.py
@@ -0,0 +1,136 @@
+
+from django.shortcuts import render
+from templates import *
+
+
+
+
+   
+def osn(request):
+    return render(request, 'MainMenu.html')  
+def Street(request):
+    return render(request, 'hz.html')
+def Scholl(request):
+    return render(request, 'school.html')
+def Index(request):
+    return render(request, 'index1.html')
+def Bayak(request):
+    return render(request, 'Bayak.html')
+def Belyaev(request):
+    return render(request, 'belyaev.html')
+def Blinov(request):
+    return render(request, 'blinov.html')
+def Volkov(request):
+    return render(request, 'volkov.html')
+def Galkina(request):
+    return render(request, 'galkina.html')
+def Glazov(request):
+    return render(request, 'glazov.html')
+def Gorodetskiy(request):
+    return render(request, 'gorodetskiy.html')
+def Gulko(request):
+    return render(request, 'gulko.html')
+def Eromasov(request):
+    return render(request, 'eromasov.html')
+def Kamenshchik(request):
+    return render(request, 'kamenshchik.html')
+def Kovalevskiy(request):
+    return render(request, 'kovalevskiy.html')
+def Kuzhetsov(request):
+    return render(request, 'kuznetsov.html')
+def Maksimova(request):
+    return render(request, 'maksimova.html')
+def Morozov(request):
+    return render(request, 'morozov.html')
+def Nikolaichev(request):
+    return render(request, 'nikolaichev.html')
+def Novichkov(request):
+    return render(request, 'novichkov.html')
+def Panchenko(request):
+    return render(request, 'panchenko.html')
+def Ragimov(request):
+    return render(request, 'ragimov.html')
+def Ratkin(request):
+    return render(request, 'ratkin.html')
+def Romanova(request):
+    return render(request, 'romanova.html')
+def Safin(request):
+    return render(request, 'safin.html')
+def Timofeeva(request):
+    return render(request, 'timofeeva.html')
+def Fedosov(request):
+    return render(request, 'fedosov.html')
+def Shalaev(request):
+    return render(request, 'shalaev.html')
+def Shevakina(request):
+    return render(request, 'shevakina.html')
+def Shokurov(request):
+    return render(request, 'shokurov.html')
+def Shutov(request):
+    return render(request, 'shutov.html')
+def Yarunin(request):
+    return render(request, 'yarunin.html')
+def Avenarius(request):
+    return render(request, 'Avenarius.html')
+def Batirev(request):
+    return render(request, 'Batirev.html')
+def Belov(request):
+    return render(request, 'Belov.html')
+def Vostrakov(request):
+    return render(request, 'Vostrakov.html')
+def Gagarin(request):
+    return render(request, 'Gagarin.html')
+def Gogol(request):
+    return render(request,'Gogol.html')
+def Gorkiy(request):
+    return render(request, 'Gorkiy.html')
+def Dzerjinski(request):
+    return render(request,'Dzerjinski.html')
+def Jukov(request):
+    return render(request,'Jukov.html')
+def Izmailov(request):
+    return render(request, 'Izmailov.html')
+def Ilyushin(request):
+    return render(request,'Ilyushin.html')
+def Kolomica(request):
+    return render(request,'Kolomica.html')
+def Korneev(request):
+    return render(request,'Korneev.html')
+def Kurizhov(request):
+    return render(request,'Kurijov.html')
+def Levanevsky(request):
+    return render(request,'Levanevsky.html')
+def Michurin(request):
+    return render(request,'Michurin.html')
+def Nevskiy(request):
+    return render(request, 'Nevskiy.html')
+def Przhevalskiy(request):
+    return render(request, 'Przhevalskiy.html')
+def Talalihin(request):
+    return render(request,'Talalihin.html')
+def Timiryazev(request):
+    return render(request,'Timiryazeva.html')
+def Tolstoy(request):
+    return render(request,'Tolstogo.html')
+def Tupolev(request):
+    return render(request,'Tupolev.html')
+def Frunze(request):
+    return render(request, 'Frunze.html')
+def Chehov(request):
+    return render(request,'Chehov.html')
+def Shevchenko(request):
+    return render(request, 'Shevchenko.html')
+def Maksimov(request):
+    return render(request,'DomLiceyN3.html')
+def School2(request):
+    return render(request,'DomSchool№2.html')
+def School9(request):
+    return render(request,'DomSchool№9.html')
+def School12(request):
+    return render(request, 'DomSchool№12.html')
+def School3(request):
+    return render(request,'IlyinskaySchool.html')
+def School4(request):
+    return render(request,'Konstantinovskaya_School.html')
+def Popa(request):
+    return render(request,'osn.html')
